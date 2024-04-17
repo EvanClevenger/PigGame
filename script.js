c9489f06888778b04e51039c8f7aa4bf0396cd18
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2024-04-17 01:51:11 -0500

    added current score functionality

--- script.js
+++ script.js
@@ -3,9 +3,38 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2. display dice.
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. check for rolled 1,if true next player
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    // switch to next player
+    currentScore = 0;
+    current0El.textContent = currentScore;
+  }
+});
